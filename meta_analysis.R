bb7c7de71f18a18b0d654c9c6876e5cd619474ee
fergusfones <ff309@exeter.ac.uk>
2025-01-15 17:21:25 +0000

    Create meta_analysis.R

---
+++ meta_analysis.R
@@ -0,0 +1,6 @@
+meta_analysis <- read.delim("/Users/fergusfones/Desktop/Nanostring/RESULTS_temporal_lobe_meta_analysis1.tsv")
+meta_analysis_processed <- read.delim("/Users/fergusfones/Desktop/Nanostring/temporal_lobe_meta-analysis_processed_results.tsv")
+#meta_analysis_info <- 
+
+View(meta_analysis_processed)
+
