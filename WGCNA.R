8b680e946a1e94018e0fdd8605707d5e4804655b
fergusfones <ff309@exeter.ac.uk>
2025-02-28 16:57:32 +0000

    Full script

--- WGCNA.R
+++ WGCNA.R
@@ -24,48 +24,91 @@ subCountData <- countDataa[-which(countDataa$TargetName %in% duplicates),]
 rownames(subCountData) <- subCountData$ProbeDisplayName
 
 
-# need to add covaraties before running WGCNA
-# do i have to control for cell types and correct the data like we did in the GeoMX workflow?
-# just use neun samples???
+
+# do i have to control for covariates like cell types and correct the data like we did in the GeoMX workflow?
+# Currently just using CA1 neun samples, current n=24
 
 
-# need to transpose data, so gene probes are in the coloumns and 'treatments' are in the rows, in a count matrix
+# need to transpose data, so gene probes are in the columns and 'treatments' are in the rows, in a count matrix
 WGCNA_matrix <- subCountData[,c(4,14:108)]
 
 WGCNA_matrix <- WGCNA_matrix %>% 
   filter(., TargetName != "NegProbe-WTX")
 
-WGCNA_matrix <- WGCNA_matrix[, grepl("neun", colnames(WGCNA_matrix)) & !grepl("rest", colnames(WGCNA_matrix)) | colnames(WGCNA_matrix) == "TargetName"]
-
 rownames(WGCNA_matrix) <- WGCNA_matrix[, 1]
-WGCNA_matrix <- WGCNA_matrix[, 2:48]
+
+WGCNA_matrix <- WGCNA_matrix[, 2:96]
 
 WGCNA_matrix <- as.matrix(WGCNA_matrix)
 
+dim(WGCNA_matrix)
+
+WGCNA_matrix_CA1_neun <- WGCNA_matrix[, grepl("neun", colnames(WGCNA_matrix)) & !grepl("rest", colnames(WGCNA_matrix)) & grepl("CA1", colnames(WGCNA_matrix)) | colnames(WGCNA_matrix) == "TargetName"]
+WGCNA_matrix_CA1_neun <- WGCNA_matrix_CA1_neun[, 2:25]
+
+
+# Normalisation of matrix via cpm
+#WGCNA_cpm <- edgeR::cpm(WGCNA_matrix)
+#WGCNA_cpm_log <- edgeR::cpm(WGCNA_matrix, log =T)
+
+
 # Normalising data with DESeq
 meta_df <- read.csv("preProcessedMetaData_filtered.csv",header = T)
-meta_df <- meta_df[, c(6, 36, 38 )]
+meta_df <- meta_df[, c(6, 32, 36, 38 )]
+
+#meta_df$age <- pathology$Month[pathology$Histology.no. %in% c("16/085", "16/095", "16/093", "16/083", "16/082", "16/092", "16/081", "16/099")]
+
+
 rownames(meta_df) <- meta_df[,1]
+
+# sorting for CA1 neun
 meta_df <- meta_df %>%
-  filter(., population == "neun")
+  filter(., population == "neun" & grepl("CA1", rownames(meta_df)))
   
 
 dds <- DESeqDataSetFromMatrix(round(WGCNA_matrix),
                               meta_df,
                               design = ~Group)
 dds <- DESeq(dds)
+# DEseq pipeline ends
+
+
+
+#vsd_cpm <- varianceStabilizingTransformation(WGCNA_cpm)
+# values are not integers
+
 vsd <- varianceStabilizingTransformation(dds)
+# what is this doing in the script?
 # linear regression fit was substituted for a local regression fit by the function
 
+
+
 wpn_vsd <- getVarianceStabilizedData(dds)
-rv_wpn <- rowVars(wpn_vsd)
+
+#wpn_vsd_cpm <- getVarianceStabilizedData(WGCNA_cpm)
+#unable to find an inherited method for function ‘dispersionFunction’ for signature ‘object = "matrix"’
+
+rv_wpn <- rowVars(wpn_vsd) 
+#rv_cpm <- rowVars(WGCNA_cpm)
 summary(rv_wpn)
 
 q75_wpn <- quantile( rowVars(wpn_vsd), .75)  # <= original
-q95_wpn <- quantile( rowVars(wpn_vsd), .95)  # <= changed to 95 quantile to reduce dataset
+q5_wpn <- quantile( rowVars(wpn_vsd), .5) 
+q95_wpn <- quantile( rowVars(wpn_vsd), .95)  # <= 95 quantile reduces dataset
 expr_normalized <- wpn_vsd[ rv_wpn > q95_wpn, ]
 dim(expr_normalized)
-# normalised expression obj showing 998 genes now...
+# normalised expression obj showing 998 genes with 95q, and 4990 with 75q
+# sometimes shows 1009??
+
+#for cpm
+#q75_cpm <- quantile( rowVars(WGCNA_cpm), .75)  # <= original
+#q5_cpm <- quantile( rowVars(WGCNA_cpm), .5) 
+#q95_cpm <- quantile( rowVars(WGCNA_cpm), .95)  # <= 95 quantile reduces dataset
+#expr_normalized_cpm <- WGCNA_cpm[ rv_cpm > q75_cpm, ]
+#dim(expr_normalized_cpm)
+# same dim as dds above
+
+#expr_normalized_cpm_log <- log(expr_normalized_cpm)
 
 expr_normalized_df <- data.frame(expr_normalized) %>%
   mutate(
@@ -87,6 +130,27 @@ expr_normalized_df %>% ggplot(., aes(x = name, y = value)) +
     y = "normalized expression"
   )
 
+# for cpm
+#expr_normalized_cpm_log_df <- data.frame(expr_normalized_cpm_log) %>%
+  mutate(
+    Gene_id = row.names(expr_normalized_cpm_log)
+  ) %>%
+  pivot_longer(-Gene_id)
+
+#expr_normalized_cpm_log_df %>% ggplot(., aes(x = name, y = value)) +
+  geom_violin() +
+  geom_point() +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text( angle = 90)
+  ) +
+  ylim(0, NA) +
+  labs(
+    title = "cpm Normalized",
+    x = "treatment",
+    y = "cpm normalized expression"
+  )
+
 # starting WGCNA
 
 input_mat <- t(expr_normalized)
@@ -99,11 +163,13 @@ powers = c(c(1:10), seq(from = 12, to = 20, by = 2))
 
 # Call the network topology analysis function
 sft = pickSoftThreshold(
-  expr_normalized,             # <= Input data
+  input_mat,             # <= Input data
   #blockSize = 30,
   powerVector = powers,
   verbose = 5
 )
+
+
 
 par(mfrow = c(1,2));
 cex1 = 0.9;
@@ -118,7 +184,8 @@ text(sft$fitIndices[, 1],
      -sign(sft$fitIndices[, 3]) * sft$fitIndices[, 2],
      labels = powers, cex = cex1, col = "red"
 )
-abline(h = 0.90, col = "red")
+abline(h = 0.80, col = "red")
+
 plot(sft$fitIndices[, 1],
      sft$fitIndices[, 5],
      xlab = "Soft Threshold (power)",
@@ -130,3 +197,169 @@ text(sft$fitIndices[, 1],
      sft$fitIndices[, 5],
      labels = powers,
      cex = cex1, col = "red")
+
+# trying power = 8
+
+picked_power <- 9
+
+temp_cor <- cor       
+cor <- WGCNA::cor         # Force it to use WGCNA cor function (fix a namespace conflict issue)
+netwk <- blockwiseModules(input_mat,                # <= input here
+                          
+                          # == Adjacency Function ==
+                          power = picked_power,                # <= power here
+                          networkType = "signed",
+                          
+                          # == Tree and Block Options ==
+                          deepSplit = 2,
+                          pamRespectsDendro = F,
+                          # detectCutHeight = 0.75,
+                          minModuleSize = 30,
+                          maxBlockSize = 4000,
+                          
+                          # == Module Adjustments ==
+                          reassignThreshold = 0,
+                          mergeCutHeight = 0.25,
+                          
+                          # == TOM == Archive the run results in TOM file (saves time)
+                          saveTOMs = T,
+                          saveTOMFileBase = "ER",
+                          
+                          # == Output Options
+                          numericLabels = T,
+                          verbose = 3)
+
+cor <- temp_cor
+
+# Convert labels to colors for plotting
+mergedColors = labels2colors(netwk$colors)
+# Plot the dendrogram and the module colors underneath
+plotDendroAndColors(
+  netwk$dendrograms[[1]],
+  mergedColors[netwk$blockGenes[[1]]],
+  "Module colors",
+  dendroLabels = FALSE,
+  hang = 0.03,
+  addGuide = TRUE,
+  guideHang = 0.05 )
+
+# netwk$colors[netwk$blockGenes[[1]]]
+# table(netwk$colors)
+
+module_df <- data.frame(
+  gene_id = names(netwk$colors),
+  colors = labels2colors(netwk$colors)
+)
+
+module_df[1:5,]
+#>            gene_id    colors
+#> 1 AC149818.2_FG001      blue
+#> 2 AC149829.2_FG003      blue
+#> 3 AC182617.3_FG001      blue
+#> 4 AC186512.3_FG001 turquoise
+#> 5 AC186512.3_FG007 turquoise
+
+write_delim(module_df,
+            file = "gene_modules.txt",
+            delim = "\t")
+
+
+# Get Module Eigengenes per cluster
+MEs0 <- moduleEigengenes(input_mat, mergedColors)$eigengenes
+
+# Reorder modules so similar modules are next to each other
+MEs0 <- orderMEs(MEs0)
+module_order = names(MEs0) %>% gsub("ME","", .)
+
+# Add treatment names
+MEs0$treatment = row.names(MEs0)
+
+# tidy & plot data
+mME = MEs0 %>%
+  pivot_longer(-treatment) %>%
+  mutate(
+    name = gsub("ME", "", name),
+    name = factor(name, levels = module_order)
+  )
+
+mME %>% ggplot(., aes(x=treatment, y=name, fill=value)) +
+  geom_tile() +
+  theme_bw() +
+  scale_fill_gradient2(
+    low = "blue",
+    high = "red",
+    mid = "white",
+    midpoint = 0,
+    limit = c(-1,1)) +
+  theme(axis.text.x = element_text(angle=90)) +
+  labs(title = "Module-trait Relationships", y = "Modules", fill="corr")
+
+
+
+# pick out a few modules of interest here
+modules_of_interest = c("grey", "yellow")
+
+# Pull out list of genes in that module
+submod = module_df %>%
+  subset(colors %in% modules_of_interest)
+
+row.names(module_df) = module_df$gene_id
+
+# Get normalized expression for those genes
+expr_normalized[1:5,1:10]
+
+subexpr = expr_normalized[submod$gene_id,]
+
+submod_df = data.frame(subexpr) %>%
+  mutate(
+    gene_id = row.names(.)
+  ) %>%
+  pivot_longer(-gene_id) %>%
+  mutate(
+    module = module_df[gene_id,]$colors
+  )
+
+submod_df %>% ggplot(., aes(x=name, y=value, group=gene_id)) +
+  geom_line(aes(color = module),
+            alpha = 0.2) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(angle = 90)
+  ) +
+  facet_grid(rows = vars(module)) +
+  labs(x = "treatment",
+       y = "normalized expression")
+
+
+genes_of_interest = module_df %>%
+  subset(colors %in% modules_of_interest)
+
+expr_of_interest = expr_normalized[genes_of_interest$gene_id,]
+#expr_of_interest[1:5,1:5]
+
+TOM = TOMsimilarityFromExpr(t(expr_of_interest),
+                            power = picked_power)
+
+# Add gene names to row and columns
+row.names(TOM) = row.names(expr_of_interest)
+colnames(TOM) = row.names(expr_of_interest)
+
+edge_list = data.frame(TOM) %>%
+  mutate(
+    gene1 = row.names(.)
+  ) %>%
+  pivot_longer(-gene1) %>%
+  dplyr::rename(gene2 = name, correlation = value) %>%
+  unique() %>%
+  subset(!(gene1==gene2)) %>%
+  mutate(
+    module1 = module_df[gene1,]$colors,
+    module2 = module_df[gene2,]$colors
+  )
+
+head(edge_list)
+
+# Export Network file to be read into Cytoscape, VisANT, etc
+write_delim(edge_list,
+            file = "edgelist.tsv",
+            delim = "\t")
