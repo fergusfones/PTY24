89fc1c0006c7192d8c2f7008c01c122eb4954a99
fergusfones <ff309@exeter.ac.uk>
2025-08-09 20:09:03 +0100

    Added EWCE, GO and WGCNA to main script

--- Nanostring.R
+++ Nanostring.R
@@ -24,6 +24,15 @@ library(dplyr)
 library(ggrepel)
 library(scater)
 library(DT)
+library(EWCE)
+library(ewceData)
+library(WGCNA)
+library(PerseusR)
+library(DESeq2)
+library(enrichR)
+mouseCTD <- ewceData::ctd()
+dbs <- listEnrichrDbs()
+dbs <- c("GO_Biological_Process_2023", "GO_Cellular_Component_2023", "GO_Molecular_Function_2023")
 
 
 metaData <- read.csv("preProcessedMetaData_filtered.csv",header = T)
@@ -477,6 +486,112 @@ de_genes_toptable_EC %>%
 
 
 ######################
+### Gene ontology - enrichr
+
+dbs <- listEnrichrDbs()
+
+head(dbs)
+
+
+dbs <- c("GO_Biological_Process_2023", "GO_Cellular_Component_2023", "GO_Molecular_Function_2023")
+
+
+rownames(de_results)
+x <- rownames(top_n(de_results, 25))
+
+spe_CA1_neun_ruv_post_enrich <- enrichr(c("Mapt",     "Car4"   ,       "Slc35f1"   ,    "Tbr1"   ,       "Hrk"         ,  "Lmbrd2"     ,   "Gfap"    ,      "Crym"     ,     "Cap1"  ,        "Celf2"     ,    "Ptprn2"  ,      "Tspan5"     ,   "App"   ,        "Cyfip2"   ,     "Acap2" ,"Grina"     ,    "Tuba1a"    ,    "Txndc15" ,      "Zbtb18"      ,  "Atp2b2"   ,     "Sncb"     ,     "Epha5"      ,   "Pcsk2"     ,    "Marcksl1"   ,   "Stim2"   ,      "Igfbp4"   ,     "Tmsb4x"    ,    "Rps24"       ,  "Slc30a3" ,      "Wasf1" ), dbs)
+spe_CA1_rest_ruv_post_enrich <- enrichr(c("Mapt"  ,    "Gfap"   ,       "Ctsd"      ,    "C4b"       ,    "Ubb"    ,       "Cyfip2"    ,    "Nrgn"    ,      "Ctss"     ,     "Itm2b"     ,    "Serpina3n"  ,   "Hrk"     ,      "Ncdn"     ,     "Calm2"   , "Zbtb18"  ,      "C1qa"      ,    "Ppp3ca"    ,    "Mt1"     ,      "Fkbp1a"   ,     "Scn1b"    ,     "Cfl1"       ,   "App"    ,       "Hexb"       ,   "Ppp3r1"   ,     "B2m"      ,     "Clu"     ,      "Capza2"   ,"Actb"    ,      "Thy1"      ,    "Actr2"   ,      "Mt2"), dbs)
+spe_EC_neun_ruv_post_enrich <- enrichr(c("Mapt"     ,     "Gfap"      ,    "Ptprn2"    ,    "Cst3"      ,    "Apoe"      ,    "Aldoa"     ,    "Smc3"      ,    "Ptgds"    ,     "Fer1l6"      ,  "Fgf14"  ,       "Esyt2"  ,      "Glul"  ,        "Slc17a7"  , "Ndrg2"    ,     "C1qa"  ,        "Aldoc"     ,    "Sncb"    ,      "Cbx8"      ,    "Mdh1"      ,    "Zwint"     ,    "Syn2"    ,      "Nap1l5"     ,   "Atp1a2"    ,    "Mt1"    ,       "Gapdh"     ,    "Nsf" , "Tmc5"    ,      "Stmn1"    ,     "Rab3a"   ,      "Atp6v0c"), dbs)
+spe_EC_rest_ruv_post_enrich <- enrichr(c("Mapt"   ,     "Gfap"    ,      "Ctsd"     ,     "Ptprn2"      ,  "Camk2n1"    ,   "C1qa"    ,      "C4b"    ,       "Ctss"      ,    "Id3"   ,        "Fer1l6"    ,    "Nrsn1"     ,    "Cst3"   ,       "Id2"    ,"Ptgds"   ,      "Ighm"    ,      "Smc3"     ,     "Cplx1"      ,   "Ankrd12"   ,    "Cbx8"    ,      "Tmsb4x"  ,      "Plp1"     ,     "C1qc"    ,      "Clu"      ,     "Cox8a"    ,     "Gm52800"     ,  "Nfasc"   , "Cd81"   ,       "Cd9"      ,     "Tyrobp"    ,    "C1qb"), dbs)
+
+plotEnrich(spe_CA1_neun_ruv_post_enrich[[1]], showTerms = 20, numChar = 120, y = "Count", orderBy = "P.value")
+plotEnrich(spe_CA1_rest_ruv_post_enrich[[3]], showTerms = 20, numChar = 120, y = "Count", orderBy = "P.value")
+plotEnrich(spe_EC_neun_ruv_post_enrich[[3]], showTerms = 20, numChar = 120, y = "Count", orderBy = "P.value")
+plotEnrich(spe_EC_rest_ruv_post_enrich[[3]], showTerms = 20, numChar = 120, y = "Count", orderBy = "P.value")
+
+
+
+#whole gene sets
+
+install.packages("tibble")
+library(tibble)
+library(tidyverse)
+
+de_genes_toptable <- topTable(fit2, coef = 1, sort.by = "p", n = Inf,p.value = 0.05,adjust.method = "fdr",lfc = 0.5)
+offTargets <- c("Wdr60", "Esyt2", "Ncapg2","Ptprn2","Fgf14")
+
+de_genes_toptable <- de_genes_toptable[-which(rownames(de_genes_toptable) %in% offTargets),]
+
+whole_spe_CA1_neun <- enrichr(rownames(de_genes_toptable), dbs)
+
+plotEnrich(whole_spe_CA1_neun[[3]], showTerms = 20, numChar = 120, y = "Count", orderBy = "P.value")
+
+
+
+
+
+
+######################
+#EWCE
+
+mouseCTD <- ewceData::ctd()
+
+#Let's use an example topTable for one brain region
+#subset significant DEG's from your toptables 
+de_genes_toptable_EC_rest <- topTable(fit2_EC_rest, coef = 1, sort.by = "p", n = Inf,p.value = 0.05,adjust.method = "fdr",lfc = 0.5)
+offTargets <- c("Wdr60", "Esyt2", "Ncapg2","Ptprn2","Fgf14")
+#Manually remove any known off-targets
+de_genes_toptable_EC_rest <- de_genes_toptable_EC_rest[-which(rownames(de_genes_toptable_EC_rest) %in% offTargets),]
+
+#finally subset the list of names for your DEGs
+topGenes <- rownames(de_genes_toptable_EC_rest)
+
+
+# This tests for enrichment in the mouse single cell dataset
+# Start out with 100 for quick running but when we run the analysis "finally" make this 10,000 reps
+results <- EWCE::bootstrap_enrichment_test(sct_data = mouseCTD,
+                                           sctSpecies = "mouse",
+                                           genelistSpecies = "mouse",
+                                           hits = topGenes, 
+                                           reps = 100,
+                                           annotLevel = 1)
+
+saveRDS(results, file = "/Users/fergusfones/Desktop/Nanostring/results_CA1_rest.Rdata")
+results_CA1_rest <- readRDS(file = "/Users/fergusfones/Desktop/Nanostring/results_CA1_rest.Rdata")
+
+saveRDS(results, file = "/Users/fergusfones/Desktop/Nanostring/results_EC_rest.Rdata")
+results_EC_rest <- readRDS(file = "/Users/fergusfones/Desktop/Nanostring/results_EC_rest.Rdata")
+
+#Take out the enrichment results
+ewceRes <- results_EC_rest$results
+
+
+# This method only tests for positive enrichment (sd_from_mean < 0 == NA)
+ewceRes[which(ewceRes$sd_from_mean < 0),"sd_from_mean"] <- NA
+
+#annotate significance
+ewceRes$sigAnnot <- NA
+ewceRes$sigAnnot[ewceRes$q < 0.05] <- "*"
+ewceRes$sigAnnot[ewceRes$q < 0.0001] <- "**"
+ewceRes$sigAnnot[ewceRes$q < 1e-10] <- "***"
+
+#Finally, plot your cell types
+ewceRes %>%
+  ggplot(aes(x = as.factor(CellType), y = sd_from_mean))+
+  geom_bar(stat = "identity")+
+  scale_y_continuous(expand = c(0,0),limits = c(0,max(ewceRes$sd_from_mean,na.rm = T) + max(ewceRes$sd_from_mean,na.rm = T)/10))+
+  geom_text(aes(label = sigAnnot, y = sd_from_mean + max(ewceRes$sd_from_mean,na.rm = T)/20), size = 8)+
+  ylab("SD From Mean")+
+  xlab("Cell type")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+
+
+
+
+
+
+######################
 # Target gene screening
 
 
@@ -517,3 +632,341 @@ sigEC <- de_results_EC[which(de_results_EC$adj.P.Val < 0.05 & abs(de_results_EC$
 
 de_results_EC[de_results_CA1$logFC < -0.5 & de_results_EC$logFC > 0.5 & de_results_EC$adj.P.Val <0.05 ,]
 
+
+
+
+
+######################
+#WGCNA
+
+WGCNA_matrix <- subCountData
+
+WGCNA_matrix <- WGCNA_matrix %>% 
+  filter(., TargetName != "NegProbe-WTX")
+
+WGCNA_matrix <- WGCNA_matrix[,14:108]
+
+WGCNA_matrix <- as.matrix(WGCNA_matrix)
+
+
+#Subsetting before normalisation
+
+
+WGCNA_matrix_CA1_neun <- WGCNA_matrix[, grepl("neun", colnames(WGCNA_matrix)) & !grepl("rest", colnames(WGCNA_matrix)) & grepl("CA1", colnames(WGCNA_matrix)) | colnames(WGCNA_matrix) == "TargetName"]
+
+
+WGCNA_matrix_CA1_rest <- WGCNA_matrix[, grepl("rest", colnames(WGCNA_matrix)) & !grepl("neun", colnames(WGCNA_matrix)) & grepl("CA1", colnames(WGCNA_matrix)) | colnames(WGCNA_matrix) == "TargetName"]
+
+
+WGCNA_matrix_EC_neun <- WGCNA_matrix[, grepl("neun", colnames(WGCNA_matrix)) & !grepl("rest", colnames(WGCNA_matrix)) & grepl("EC", colnames(WGCNA_matrix)) | colnames(WGCNA_matrix) == "TargetName"]
+
+
+WGCNA_matrix_EC_rest <- WGCNA_matrix[, grepl("rest", colnames(WGCNA_matrix)) & !grepl("neun", colnames(WGCNA_matrix)) & grepl("EC", colnames(WGCNA_matrix)) | colnames(WGCNA_matrix) == "TargetName"]
+
+
+meta_df_WGCNA <- metaData[, c(6, 32, 35, 36, 38 )]
+
+# sorting for CA1 neun
+meta_df_CA1_neun <- meta_df_WGCNA %>%
+  filter(., population == "neun" & grepl("CA1", rownames(meta_df_WGCNA)))
+
+# sorting for CA1 rest
+meta_df_CA1_rest <- meta_df_WGCNA %>%
+  filter(., population == "rest" & grepl("CA1", rownames(meta_df_WGCNA)))
+
+# sorting for EC neun
+meta_df_EC_neun <- meta_df_WGCNA %>%
+  filter(., population == "neun" & grepl("EC", rownames(meta_df_WGCNA)))
+
+# sorting for EC rest
+meta_df_EC_rest <- meta_df_WGCNA %>%
+  filter(., population == "rest" & grepl("EC", rownames(meta_df_WGCNA)))
+
+
+dds <- DESeqDataSetFromMatrix(round(WGCNA_matrix_CA1_neun),
+                              meta_df_CA1_neun,
+                              design = ~Group)
+dds <- DESeq(dds)
+
+vsd <- varianceStabilizingTransformation(dds)
+# retains metadata, can be used for PCA, visualisation, further processing
+# linear regression fit was substituted for a local regression fit by the function
+
+wpn_vsd <- getVarianceStabilizedData(dds)
+
+rv_wpn <- rowVars(wpn_vsd) 
+summary(rv_wpn)
+
+q75_wpn <- quantile( rowVars(wpn_vsd), .75)  # <= original
+q95_wpn <- quantile( rowVars(wpn_vsd), .95)  # <= changed to 95 quantile to reduce dataset
+expr_normalized <- wpn_vsd[ rv_wpn > q75_wpn, ]
+
+
+expr_normalized_df <- data.frame(expr_normalized) %>%
+  mutate(
+    Gene_id = row.names(expr_normalized)
+  ) %>%
+  pivot_longer(-Gene_id)
+
+expr_normalized_df %>% ggplot(., aes(x = name, y = value)) +
+  geom_violin() +
+  geom_point() +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text( angle = 90)
+  ) +
+  ylim(0, NA) +
+  labs(
+    title = "Normalized and 75 quantile Expression_EC_rest",
+    x = "treatment",
+    y = "normalized expression"
+  )
+
+
+
+# hierarchical clustering 
+
+htree <- hclust(dist(t(expr_normalized)))
+plot(htree, xlab = "samples", main = "hierarchical clustering ")
+
+input_mat <- t(expr_normalized)
+
+# Allowing multi-threading
+allowWGCNAThreads() 
+
+# Choose a set of soft-thresholding powers
+powers = c(c(1:10), seq(from = 12, to = 30, by = 2))
+
+# Call the network topology analysis function
+sft = pickSoftThreshold(
+  input_mat,             # <= Input data
+  #blockSize = 30,
+  powerVector = powers,
+  verbose = 5
+)
+
+
+par(mfrow = c(1,2));
+cex1 = 0.9;
+plot(sft$fitIndices[, 1],
+     -sign(sft$fitIndices[, 3]) * sft$fitIndices[, 2],
+     xlab = "Soft Threshold (power)",
+     ylab = "Scale Free Topology Model Fit, signed R^2",
+     main = paste("Scale independence")
+)
+text(sft$fitIndices[, 1],
+     -sign(sft$fitIndices[, 3]) * sft$fitIndices[, 2],
+     labels = powers, cex = cex1, col = "red",
+)
+
+abline(h = 0.8, col = "red")
+plot(sft$fitIndices[, 1],
+     sft$fitIndices[, 5],
+     xlab = "Soft Threshold (power)",
+     ylab = "Mean Connectivity",
+     type = "n",
+     main = paste("Mean connectivity")
+)
+text(sft$fitIndices[, 1],
+     sft$fitIndices[, 5],
+     labels = powers,
+     cex = cex1, col = "red")
+
+picked_power <- 5
+
+temp_cor <- cor       
+cor <- WGCNA::cor         # Force it to use WGCNA cor function (fix a namespace conflict issue)
+netwk <- blockwiseModules(input_mat,                # <= input here
+                          
+                          # == Adjacency Function ==
+                          power = picked_power,                # <= power here
+                          networkType = "signed",
+                          
+                          # == Tree and Block Options ==
+                          deepSplit = 2,
+                          pamRespectsDendro = F,
+                          # detectCutHeight = 0.75,
+                          minModuleSize = 30,
+                          maxBlockSize = 4000,
+                          
+                          # == Module Adjustments ==
+                          reassignThreshold = 0,
+                          mergeCutHeight = 0.25,
+                          
+                          # == TOM == Archive the run results in TOM file (saves time)
+                          saveTOMs = T,
+                          saveTOMFileBase = "ER",
+                          
+                          # == Output Options
+                          numericLabels = T,
+                          verbose = 3)
+
+cor <- temp_cor
+
+
+
+
+# Convert labels to colors for plotting
+mergedColors = labels2colors(netwk$colors)
+
+# Plot the dendrogram and the module colors underneath
+plotDendroAndColors(
+  netwk$dendrograms[[1]],
+  mergedColors[netwk$blockGenes[[1]]],
+  "Module colors",
+  dendroLabels = FALSE,
+  hang = 0.03,
+  addGuide = TRUE,
+  guideHang = 0.05 )
+
+netwk$colors[netwk$blockGenes[[1]]]
+table(netwk$colors)
+
+module_df <- data.frame(
+  gene_id = names(netwk$colors),
+  colors = labels2colors(netwk$colors)
+)
+
+module_df[1:5,]
+
+MEs0 <- moduleEigengenes(input_mat, mergedColors)$eigengenes
+MEs0 <- orderMEs(MEs0)
+module_order = names(MEs0) %>% gsub("ME","", .)
+
+# Adding other variables
+MEs0 <- cbind(MEs0, meta_df_CA1_neun)
+
+### Remove grey module (junk)
+MEs0 <- MEs0[,-which(colnames(MEs0)=="MEgrey")]
+
+traits <- meta_df_CA1_neun %>%
+  select(- c("SegmentDisplayName", "Histology.no.", "population", "grossRegion"))
+
+#encoding traits
+# do as factor
+traits$Group <- as.factor(traits$Group)
+
+traits_numeric <- ifelse(traits == "WW", 0 ,1)
+
+# module trait correlation
+# adjust rows in MEs0 to just show modules
+moduleTraitCor = stats::cor(MEs0[,1:33],traits_numeric , use = "p");
+moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nrow(MEs0))
+
+
+textMatrix = paste(signif(moduleTraitCor, 2), "\n(", signif(moduleTraitPvalue, 1), ")", sep = "")
+dim(textMatrix) = dim(moduleTraitCor)
+
+# Adjust margins and font sizes
+par(mfrow = c(1, 1))
+par(mar = c(2, 2, 3, 3), cex.main = 1.5, cex.axis = 1, cex.lab = 0.7)  # Decrease margins and adjust font sizes
+
+# Plot the heatmap
+
+#pheatmap(moduleTraitCor, col = colours, main = "module trait correlation")
+
+
+labeledHeatmap(
+  Matrix = moduleTraitCor,
+  xLabels = colnames(traits_numeric),
+  yLabels = names(MEs0[,1:33]),
+  ySymbols = names(MEs0[,1:33]),
+  colorLabels = FALSE,
+  colors = colorRampPalette(c("blue", "white", "red"))(50),  # Change color scheme if needed
+  textMatrix = textMatrix,
+  setStdMargins = TRUE,
+  plotLegend = TRUE,
+  cex.text = 0.6,  # Reduce label size
+  zlim = c(-1, 1),
+  main = "Module-trait relationships"
+)
+
+# how many genes in each module
+table(module_df$colors)
+
+# pick out a few modules of interest here
+modules_of_interest = c("brown", "turquoise", "red", "blue", "magenta" )
+
+# Pull out list of genes in that module
+submod = module_df %>%
+  subset(colors %in% modules_of_interest)
+
+row.names(module_df) = module_df$gene_id
+
+# Get normalized expression for those genes
+subexpr = expr_normalized[submod$gene_id,]
+
+submod_df = data.frame(subexpr) %>%
+  mutate(
+    gene_id = row.names(.)
+  ) %>%
+  pivot_longer(-gene_id) %>%
+  mutate(
+    module = module_df[gene_id,]$colors
+  )
+
+submod_df %>% ggplot(., aes(x=name, y=value, group=gene_id)) +
+  geom_line(aes(color = module),
+            alpha = 0.2) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(angle = 90)
+  ) +
+  facet_grid(rows = vars(module)) +
+  labs(x = "treatment",
+       y = "normalized expression")
+
+module_df[module_df$gene_id == "Gfap",]
+module_df[module_df$gene_id == "Pfkm",]
+module_df[module_df$gene_id == "Mapt",]
+
+module_df$gene_id <- sub("_01$", "", module_df$gene_id)
+
+WGCNA_brown <-module_df$gene_id[module_df$colors == "brown"]
+WGCNA_black <-module_df$gene_id[module_df$colors == "black"]
+
+module_enriched <- enrichr(WGCNA_black, dbs)
+
+# maybe add p value threshold at 0.05
+
+plotEnrich(module_enriched[[2]], showTerms = 20, numChar = 120, y = "Count", orderBy = "P.value")
+
+
+
+#finally subset the list of names for your DEGs
+topGenes <- WGCNA_brown
+
+
+# This tests for enrichment in the mouse single cell dataset
+# Start out with 100 for quick running but when we run the analysis "finally" make this 10,000 reps
+results <- EWCE::bootstrap_enrichment_test(sct_data = mouseCTD,
+                                           sctSpecies = "mouse",
+                                           genelistSpecies = "mouse",
+                                           hits = topGenes, 
+                                           reps = 10000,
+                                           annotLevel = 1)
+
+
+#Take out the enrichment results
+ewceRes <- results$results
+
+
+# This method only tests for positive enrichment (sd_from_mean < 0 == NA)
+ewceRes[which(ewceRes$sd_from_mean < 0),"sd_from_mean"] <- NA
+
+#annotate significance
+ewceRes$sigAnnot <- NA
+ewceRes$sigAnnot[ewceRes$q < 0.05] <- "*"
+ewceRes$sigAnnot[ewceRes$q < 0.0001] <- "**"
+ewceRes$sigAnnot[ewceRes$q < 1e-10] <- "***"
+
+#Finally, plot your cell types
+ewceRes %>%
+  ggplot(aes(x = as.factor(CellType), y = sd_from_mean))+
+  geom_bar(stat = "identity")+
+  scale_y_continuous(expand = c(0,0),limits = c(0,max(ewceRes$sd_from_mean,na.rm = T) + max(ewceRes$sd_from_mean,na.rm = T)/10))+
+  geom_text(aes(label = sigAnnot, y = sd_from_mean + max(ewceRes$sd_from_mean,na.rm = T)/20), size = 8)+
+  ylab("SD From Mean")+
+  xlab("Cell type")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
